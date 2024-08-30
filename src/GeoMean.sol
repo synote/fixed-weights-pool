06d87c7752d321619c7eadac9b3855c7f405491f
simon <hesynote@gmail.com>
2024-08-30 16:48:19 +0800

    add poc swap function for weighted pool

--- src/GeoMean.sol
+++ src/GeoMean.sol
@@ -4,15 +4,22 @@ pragma solidity ^0.8.24;
 import {BaseHook} from "v4-periphery/src/base/hooks/BaseHook.sol";
 
 import {Hooks} from "v4-core/src/libraries/Hooks.sol";
+import {SafeCast} from "v4-core/src/libraries/SafeCast.sol";
 import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
 import {PoolKey} from "v4-core/src/types/PoolKey.sol";
+import {Currency} from "v4-core/src/types/Currency.sol";
 import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
 import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
-import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "v4-core/src/types/BeforeSwapDelta.sol";
+import {BeforeSwapDelta, toBeforeSwapDelta} from "v4-core/src/types/BeforeSwapDelta.sol";
+import {CurrencySettler} from "v4-core/test/utils/CurrencySettler.sol";
 import {WeightMath} from "./WeightMath.sol";
 
 contract Counter is BaseHook {
     using PoolIdLibrary for PoolKey;
+    using CurrencySettler for Currency;
+    using SafeCast for *;
+
+    error SwapNotImplemented();
 
     // NOTE: ---------------------------------------------------------
     // state variables should typically be unique to a pool
@@ -24,6 +31,17 @@ contract Counter is BaseHook {
 
     mapping(PoolId => uint256 count) public beforeAddLiquidityCount;
     mapping(PoolId => uint256 count) public beforeRemoveLiquidityCount;
+
+    // Fixed weights for pool reserve
+    // The custom pool use defualt 50% : 50% ration for the poc project.
+    struct PoolParams {
+        uint64 weightX;
+        uint64 weightY;
+        uint256 reserveX;
+        uint256 reserveY;
+    }
+
+    mapping(PoolId => PoolParams) public poolWeights;
 
     constructor(IPoolManager _poolManager) BaseHook(_poolManager) {}
 
@@ -50,16 +68,104 @@ contract Counter is BaseHook {
     // NOTE: see IHooks.sol for function documentation
     // -----------------------------------------------
 
+    function getPoolParams(PoolId key)
+        public
+        view
+        returns (uint64, uint64)
+    {
+        return (poolWeights[key].weightX, poolWeights[key].weightY);
+    }
+
     function beforeSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata params, bytes calldata)
         external
         override
         returns (bytes4, BeforeSwapDelta, uint24)
     {
+        // convert the amountSpecified to positive
+        uint256 amountPositive = params.amountSpecified > 0
+            ? uint256(params.amountSpecified)
+            : uint256(-params.amountSpecified);
 
-        if (params.zeroForOne) {}
+        PoolParams memory wpool = poolWeights[key.toId()];
+        BeforeSwapDelta beforeSwapDelta;
 
+        // settle balance to hook
+        if (params.zeroForOne) {
+            if (params.amountSpecified > 0) {
+                // exact output not implemented
+                revert SwapNotImplemented();
+            }
+            // exact input
+            uint256 amountOut = WeightMath.calcAmountOut(
+                amountPositive,
+                wpool.reserveX,
+                wpool.reserveY,
+                0.5 ether,
+                0.5 ether
+            );
+
+            wpool.reserveX += amountPositive;
+            wpool.reserveY -= amountOut;
+            // mint 6909 claim for hook
+            key.currency0.take(
+                poolManager,
+                address(this),
+                amountPositive,
+                true
+            );
+
+            // burn 6909 claim from hook
+            key.currency1.settle(
+                poolManager,
+                address(this),
+                amountOut,
+                true
+            );
+
+            beforeSwapDelta = toBeforeSwapDelta(
+                int128(-params.amountSpecified),
+                -amountOut.toInt128()
+            );
+        } else {
+            if (params.amountSpecified > 0) {
+                // exact output not implemented
+                revert SwapNotImplemented();
+            }
+            // exact input
+            uint256 amountOut = WeightMath.calcAmountOut(
+                amountPositive,
+                wpool.reserveY,
+                wpool.reserveX,
+                0.5 ether,
+                0.5 ether
+            );
+            // state changed by swap
+            wpool.reserveY += amountPositive;
+            wpool.reserveX -= amountOut;
+            // mint 6909 claim for hook
+            key.currency1.take(
+                poolManager,
+                address(this),
+                amountPositive,
+                true
+            );
+            // burn 6909 claim from hook
+            key.currency0.settle(
+                poolManager,
+                address(this),
+                amountOut,
+                true
+            );
+
+            beforeSwapDelta = toBeforeSwapDelta(
+                int128(-params.amountSpecified),
+                -amountOut.toInt128()
+            );
+        }
+
+        // calcAmountOut
         beforeSwapCount[key.toId()]++;
-        return (BaseHook.beforeSwap.selector, BeforeSwapDeltaLibrary.ZERO_DELTA, 0);
+        return (BaseHook.beforeSwap.selector, beforeSwapDelta, 0);
     }
 
     function afterSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata, BalanceDelta, bytes calldata)
--- src/WeightMath.sol
+++ src/WeightMath.sol
@@ -1,12 +1,52 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {FixedPointMathLib} from "solady/src/utils/FixedPointMathLib.sol";
-import {SafeCastLib} from "solady/src/utils/SafeCastLib.sol";
+import "solady/src/utils/FixedPointMathLib.sol";
+import "solady/src/utils/SafeCastLib.sol";
 
 library WeightMath {
-    using FixedPointMathLib for *;
+    /// -----------------------------------------------------------------------
+    /// Dependencies
+    /// -----------------------------------------------------------------------
+
     using SafeCastLib for *;
+
+    using FixedPointMathLib for *;
+
+    /// -----------------------------------------------------------------------
+    /// Errors
+    /// -----------------------------------------------------------------------
+
+    /// @dev Thrown when `amountIn` exceeds `MAX_PERCENTAGE_IN`, which is imposed by balancer.
+    error AmountInTooLarge();
+
+    /// @dev Thrown when `amountOut` exceeds `MAX_PERCENTAGE_OUT`, which is imposed by balancer.
+    error AmountOutTooLarge();
+
+    /// -----------------------------------------------------------------------
+    /// Constants
+    /// -----------------------------------------------------------------------
+
+    /// @dev Maximum relative error allowed for fixed-point math operations (10^(-14)).
+    uint256 internal constant MAX_POW_RELATIVE_ERROR = 10000;
+
+    /// @dev Maximum percentage of reserveIn allowed to be swapped in when using `getAmountOut` (30%).
+    uint256 internal constant MAX_PERCENTAGE_IN = 0.3 ether;
+
+    /// @dev Maximum percentage of reserveOut allowed to be swapped out when using `getAmountIn` (30%).
+    uint256 internal constant MAX_PERCENTAGE_OUT = 0.3 ether;
+
+    function calcSpotPrice(
+        uint256 rX,
+        uint256 rY,
+        uint256 weightX,
+        uint256 weightY
+    ) internal pure returns (uint256) {
+        // -----------------------------------------------------------------------
+        // (reserveIn / weightIn) / (reserveOut / weightOut)
+        // -----------------------------------------------------------------------
+
+        return rX.divWad(weightX).divWad(rY.divWad(weightY));
+    }
 
     function calcInvariant(
         uint256 rX,
@@ -14,15 +54,63 @@ library WeightMath {
         uint256 weightX,
         uint256 weightY
     ) internal pure returns (uint256 invariant) {
-        //
-        // rX^wX
-        // rY^wY
-        //
+        // -----------------------------------------------------------------------
+        //   ____
+        //   ⎟⎟          weight
+        //   ⎟⎟  reserve ^     = i
+        //   n = 2
+        // -----------------------------------------------------------------------
+
         invariant = 1e18.mulWad(powWad(rX, weightX)).mulWad(
             powWad(rY, weightY)
         );
     }
 
+    /// @notice Calculate the amount of input asset required to get a specific amount of output asset from the pool.
+    /// @param amountOut The desired amount of output asset.
+    /// @param reserveIn The reserve of the input asset in the pool.
+    /// @param reserveOut The reserve of the output asset in the pool.
+    /// @param weightIn The weight of the input asset in the pool.
+    /// @param weightOut The weight of the output asset in the pool.
+    function calcAmountIn(
+        uint256 amountOut,
+        uint256 reserveIn,
+        uint256 reserveOut,
+        uint256 weightIn,
+        uint256 weightOut
+    ) internal pure returns (uint256) {
+        unchecked {
+            // -----------------------------------------------------------------------
+            //
+            //             ⎛                       ⎛weightIn ⎞    ⎞
+            //             ⎜                        ─────────      ⎟
+            //             ⎜                       ⎝weightOut⎠    ⎟
+            //             ⎜⎛     reserveOut      ⎞               ⎟
+            // reserveIn ⋅    ─────────────────────             - 1
+            //             ⎝⎝reserveOut - amountIn⎠               ⎠
+            // -----------------------------------------------------------------------
+
+            // Assert `amountOut` cannot exceed `MAX_PERCENTAGE_OUT`.
+            if (amountOut > reserveOut.mulWad(MAX_PERCENTAGE_OUT)) {
+                revert AmountOutTooLarge();
+            }
+
+            // `MAX_PERCENTAGE_OUT` check ensures `amountOut` is always less than `reserveOut`.
+            return reserveIn.mulWadUp(
+                powWadUp(
+                    reserveOut.divWadUp(reserveOut.rawSub(amountOut)),
+                    weightOut.divWadUp(weightIn)
+                ) - 1 ether
+            );
+        }
+    }
+
+    /// @notice Calculate the amount of output asset received by providing a specific amount of input asset to the pool.
+    /// @param amountIn The amount of input asset provided.
+    /// @param reserveIn The reserve of the input asset in the pool.
+    /// @param reserveOut The reserve of the output asset in the pool.
+    /// @param weightIn The weight of the input asset in the pool.
+    /// @param weightOut The weight of the output asset in the pool.
     function calcAmountOut(
         uint256 amountIn,
         uint256 reserveIn,
@@ -40,6 +128,11 @@ library WeightMath {
         //             ⎝    ⎝reserveIn + amountIn⎠           ⎠
         // -----------------------------------------------------------------------
 
+        // Assert `amountIn` cannot exceed `MAX_PERCENTAGE_IN`.
+        if (amountIn > reserveIn.mulWad(MAX_PERCENTAGE_IN)) {
+            revert AmountInTooLarge();
+        }
+
         return reserveOut.mulWad(
             1e18.rawSub(
                 powWadUp(
@@ -50,28 +143,31 @@ library WeightMath {
         );
     }
 
-    function calcAmountIn(
-        uint256 amountOut,
-        uint256 reserveIn,
-        uint256 reserveOut,
-        uint256 weightIn,
-        uint256 weightOut
-    ) internal pure returns (uint256) {
-        // -----------------------------------------------------------------------
-        //
-        //             ⎛                       ⎛weightIn ⎞    ⎞
-        //             ⎜                        ─────────      ⎟
-        //             ⎜                       ⎝weightOut⎠    ⎟
-        //             ⎜⎛     reserveOut      ⎞               ⎟
-        // reserveIn ⋅    ─────────────────────             - 1
-        //             ⎝⎝reserveOut - amountIn⎠               ⎠
-        // -----------------------------------------------------------------------
+    function powWad(uint256 x, uint256 y) internal pure returns (uint256) {
+        if (y == 1 ether) {
+            return x;
+        } else if (y == 2 ether) {
+            return x.mulWad(x);
+        } else if (y == 4 ether) {
+            uint256 square = x.mulWad(x);
+            return square.mulWad(square);
+        }
 
-        return reserveIn.mulWadUp(
-            powWadUp(
-                reserveOut.divWadUp(reserveOut.rawSub(amountOut)),
-                weightOut.divWadUp(weightIn)
-            ) - 1 ether
-        );
+        return int256(x).powWad(int256(y)).toUint256();
+    }
+
+    function powWadUp(uint256 x, uint256 y) internal pure returns (uint256) {
+        if (y == 1 ether) {
+            return x;
+        } else if (y == 2 ether) {
+            return x.mulWadUp(x);
+        } else if (y == 4 ether) {
+            uint256 square = x.mulWadUp(x);
+            return square.mulWadUp(square);
+        }
+
+        uint256 power = int256(x).powWad(int256(y)).toUint256();
+
+        return power + power.mulWadUp(MAX_POW_RELATIVE_ERROR) + 1;
     }
 }
